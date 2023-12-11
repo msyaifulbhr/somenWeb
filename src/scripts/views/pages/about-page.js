2eb8a1bfbe48a271005e3262a53804356d9ba0fb
msyaifulbhr <syaifulyy@gmail.com>
2023-12-11 22:48:46 +0700

    add list team

--- src/scripts/views/pages/about-page.js
+++ src/scripts/views/pages/about-page.js
@@ -15,6 +15,85 @@ const About = {
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
  
 </head>
+
+<div class="container mt-70">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="col-lg-12 mb-20">
+                    <div class="col-lg-2 team-item">
+                        <div class="card">
+                            <img src="images/default.jpg" class="avatar" alt="Eldy">
+                        </div>
+                        <div class="col-lg-10">
+                            <div class="card-body">
+                                <h5 class="card-title">Eldy Efendy</h5>
+                                <p class="card-text">Front-End Developer</p>
+                                <a href="#" class="btn btn-primary">Lihat Profil</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12 mb-20">
+                    <div class="col-lg-2 team-item">
+                        <div class="card">
+                            <img src="images/default.jpg" class="avatar" alt="Ivan">
+                        </div>
+                        <div class="col-lg-10">
+                            <div class="card-body">
+                                <h5 class="card-title">Ivan Prastio</h5>
+                                <p class="card-text">Back-End Developer</p>
+                                <a href="#" class="btn btn-primary">Lihat Profil</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="col-lg-12 mb-20">
+                    <div class="col-lg-2 team-item">
+                        <div class="card">
+                            <img src="images/default.jpg" class="avatar" alt="Fikri">
+                        </div>
+                        <div class="col-lg-10">
+                            <div class="card-body">
+                                <h5 class="card-title">Fikri Haikal Dwi Putra Chaniago</h5>
+                                <p class="card-text">Front-End Developer</p>
+                                <a href="#" class="btn btn-primary">Lihat Profil</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12 mb-20">
+                    <div class="col-lg-2 team-item">
+                        <div class="card">
+                            <img src="images/default.jpg" class="avatar" alt="Syaiful">
+                        </div>
+                        <div class="col-lg-10">
+                            <div class="card-body">
+                                <h5 class="card-title">Muhamad Syaiful Bahri</h5>
+                                <p class="card-text">Front-End Developer</p>
+                                <a href="#" class="btn btn-primary">Lihat Profil</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12 mb-20">
+                    <div class="col-lg-2 team-item">
+                        <div class="card">
+                            <img src="images/default.jpg" class="avatar" alt="Ahmad">
+                        </div>
+                        <div class="col-lg-10">
+                            <div class="card-body">
+                                <h5 class="card-title">Ahmad Fauzi Ariyanto</h5>
+                                <p class="card-text">Front-End Developer</p>
+                                <a href="#" class="btn btn-primary">Lihat Profil</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
           `;
   },
 
